cb86765955cafd4fc4795180a816d7b2070766f3
Segnicho <segnidesta60@gmail.com>
2023-09-04 16:37:54 +0300

    first commit

---
+++ server/index.ts
@@ -0,0 +1,8 @@
+import fs from "fs";;
+console.log("🚀 ~ file: index.ts:2 ~ 2:", 2)
+const addNumbers = (a: number, b: number):number => {
+    return a + b
+}
+
+const res = addNumbers(3, 5)
+console.log("🚀 ~ file: index.ts:7 ~ res:", res)
